8a2841b36dbe7f2c0a80456bf89e444776d0c4f2
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2024-11-03 17:42:20 +0100

    📈 display users on mongoDB compas  📈

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.useGlobalPipes(new ValidationPipe()); permet une validation des données entrer
   await app.listen(3000);
 }
 bootstrap();
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,20 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { UserService } from './users.service';
+import { CreateUserDto } from './dto/CreateUser.dto';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UserService) {}
   @Post()
-  createUsers() {}
+  @UsePipes(new ValidationPipe())
+  createUsers(@Body() createUserDto: CreateUserDto) {
+    console.log(createUserDto);
+    return this.usersService.createUser(createUserDto);
+  }
 }
